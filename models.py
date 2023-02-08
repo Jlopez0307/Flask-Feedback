e0e0cc016b862a33c85ae9d853bccca2018e66cf
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2023-02-08 00:19:20 -0500

    Added routes for registration and a secret route for registered users;

--- models.py
+++ models.py
@@ -8,3 +8,30 @@ bcrypt = Bcrypt()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key = True, unique = True, nullable = False)
+    password = db.Column(db.Text, nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    first_name = db.Column(db.String(30), nullable = False)
+    last_name = db.Column(db.String(30), nullable = False)
+
+    @classmethod
+    def register(cls, username, pwd, email , first_name , last_name):
+        hashed = bcrypt.generate_password_hash(pwd)
+        hashed_utf8 = hashed.decode("utf8")
+        return cls(username = username, password = hashed_utf8, email = email, first_name = first_name, last_name = last_name)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        # Validate that user exists and password is correct
+        u = User.query.filter_by(username = username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # Return user instance
+            return u
+        else: 
+            return False
